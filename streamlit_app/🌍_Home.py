c1d7aa8d95f21d986af8aebe065f023814bd77fe
Edwin George <edwingeorge521@gmail.com>
2026-03-23 00:44:20 +0300

    Renamed app.py to Home for sidebar label

--- streamlit_app/🌍_Home.py
+++ streamlit_app/🌍_Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="African Fiscal Intelligence",
+    page_title="🌍 Home · African Fiscal Intelligence",
     page_icon="🌍",
     layout="wide",
     initial_sidebar_state="expanded"
